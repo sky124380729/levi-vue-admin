947ec90b79bfb246e833f5e50934e31029a413db
Levi <jerry.zheng>
2021-01-22 17:27:57 +0800

    JSX绑定自定义事件报错问题修复

--- src/shims.d.ts
+++ src/shims.d.ts
@@ -5,5 +5,19 @@ declare module '*.vue' {
     export default component
 }
 
+declare namespace JSX {
+    // interface ElementAttributesProperty {
+    //     $props: any
+    // }
+    // interface IntrinsicElements {
+    //     [elem: string]: any
+    // }
+
+    // avoid JSX 绑定自定义事件报错
+    interface IntrinsicAttributes {
+        [elem: string]: any
+    }
+}
+
 // for tsx
 declare const React: string
