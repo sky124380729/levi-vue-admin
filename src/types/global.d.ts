441c00ebc95b2c50e854f2a44bf55c57237d9fa8
Levi <jerry.zheng>
2021-01-25 16:16:18 +0800

    增加全局类型

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -5,3 +5,16 @@ declare interface Fn<T = any, R = T> {
 declare type Nullable<T> = T | null
 
 declare type ElRef<T extends HTMLElement = HTMLDivElement> = Nullable<T>
+
+declare interface Pagination<T> {
+    current: number
+    query?: T
+    size: number
+}
+
+declare interface Res<T> {
+    code: number
+    data: T
+    message: string
+    success: boolean
+}
