010d2253b4c4ed0ef789b41eef70dbc346d5b724
Levi <jerry.zheng>
2021-01-26 14:14:03 +0800

    FormModal

---
+++ src/components/FormModal/index.tsx
@@ -0,0 +1,126 @@
+import { computed, defineComponent, PropType, ref, unref } from 'vue'
+import { Modal, Form, Row, Col } from 'ant-design-vue'
+
+type Column = 1 | 2 | 3
+
+export default defineComponent({
+    name: 'Levi-ModalForm',
+    inheritAttrs: false,
+    props: {
+        visible: {
+            type: Boolean,
+            required: true
+        },
+        form: {
+            required: true,
+            type: Object
+        },
+        loading: {
+            type: Boolean
+        },
+        column: {
+            type: Number as PropType<Column>,
+            default: 2
+        }
+    },
+    emits: ['submit', 'update:visible', 'update:form'],
+    setup(props, { emit, attrs, slots }) {
+        const formRef = ref<any>(null)
+        const visibleRef = ref(
+            computed({
+                get: () => props.visible,
+                set: (val) => {
+                    emit('update:visible', val)
+                }
+            })
+        )
+        const modelRef = ref(
+            computed({
+                get: () => props.form,
+                set: (val) => {
+                    emit('update:form', val)
+                }
+            })
+        )
+        const modalAfterClose = () => {
+            formRef.value && formRef.value.clearValidate()
+            modelRef.value = {}
+        }
+        const getModalAttrs = () => {
+            const o = Object.create(null)
+            const keys = ['title', 'width']
+            keys.forEach((key) => {
+                o[key] = attrs[key]
+            })
+            return o
+        }
+        const getFormAttrs = () => {
+            const o = Object.create(null)
+            const keys = ['rules']
+            keys.forEach((key) => {
+                o[key] = attrs[key]
+            })
+            return o
+        }
+
+        const onOk = () => {
+            formRef.value
+                .validate()
+                .then(() => {
+                    emit('submit')
+                })
+                .catch(() => null)
+        }
+        const onCancel = () => {
+            visibleRef.value = false
+        }
+
+        const renderFormItems = () => {
+            const { column } = props
+            if (slots && typeof slots.default === 'function') {
+                return slots.default().map((col) => {
+                    return <Col span={24 / column}>{col}</Col>
+                })
+            }
+            return null
+        }
+
+        const widthRef = computed(() => {
+            const { column } = props
+            const map: any = {
+                1: 480,
+                2: 750,
+                3: 980
+            }
+            return map[column]
+        })
+        return () => {
+            const { loading } = props
+            return (
+                <div class='levi-modalForm'>
+                    <Modal
+                        {...getModalAttrs()}
+                        confirmLoading={loading}
+                        width={unref(widthRef)}
+                        visible={unref(visibleRef)}
+                        afterClose={modalAfterClose}
+                        onOk={onOk}
+                        onCancel={onCancel}
+                    >
+                        <Form
+                            {...getFormAttrs()}
+                            labelCol={{ span: 6 }}
+                            wrapperCol={{ span: 16 }}
+                            labelAlign='right'
+                            scrollToFirstError={true}
+                            ref={formRef}
+                            model={modelRef.value}
+                        >
+                            <Row>{renderFormItems()}</Row>
+                        </Form>
+                    </Modal>
+                </div>
+            )
+        }
+    }
+})
--- src/views/sys/Homepage.vue
+++ src/views/sys/Homepage.vue
@@ -1,13 +1,49 @@
 <template>
-    <div>homepage</div>
+    <div>
+        <FormModal v-model:form="form" v-model:visible="visible" title="测试" :rules="rules" :loading="loading" @submit="submit">
+            <a-form-item label="登录名" name="username">
+                <a-input v-model:value="form.username" />
+            </a-form-item>
+            <a-form-item label="姓名" name="name">
+                <a-input v-model:value="form.name" />
+            </a-form-item>
+            <a-form-item label="姓名" name="name">
+                <a-input v-model:value="form.name" />
+            </a-form-item>
+            <a-form-item label="姓名" name="name">
+                <a-input v-model:value="form.name" />
+            </a-form-item>
+        </FormModal>
+        <a-button type="primary" @click="show">开启</a-button>
+    </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref, reactive } from 'vue'
 
+import FormModal from '/@/components/FormModal'
 export default defineComponent({
+    components: {
+        FormModal
+    },
     setup() {
-        return {}
+        const visible = ref<boolean>(false)
+        const loading = ref<boolean>(false)
+        const rules = {
+            username: { required: true, type: 'string' }
+        }
+        const form = reactive<any>({})
+        const show = () => {
+            visible.value = true
+        }
+        const submit = () => {
+            loading.value = true
+            setTimeout(() => {
+                loading.value = false
+                visible.value = false
+            }, 2000)
+        }
+        return { rules, form, visible, show, submit, loading }
     }
 })
 </script>
