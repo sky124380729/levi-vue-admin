d1e9cb7bae6886393645fb2a37ec77f68582b62e
lockingreal <1191515088@qq.com>
2021-02-10 13:18:19 +0800

    新增录录屏功能

---
+++ src/views/buffer/screenrecorder.ts
@@ -0,0 +1,13 @@
+export interface mediaOptions {
+    mediaRecorder: any;
+    data: [];
+    stream: any;
+    checked: boolean;
+    isStart: boolean;
+    isFinish: boolean;
+    [key: string]: any;
+}
+
+export interface mediaData {
+    data:never;
+}
