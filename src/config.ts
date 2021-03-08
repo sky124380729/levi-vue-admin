69f7ec0e670cdf4e3f701ced9561c0ad5a276b30
Levi <124380729@qq.com>
2021-03-08 10:37:01 +0800

    config bug fixed

--- src/config.ts
+++ src/config.ts
@@ -5,10 +5,10 @@ export const BASE_URL = import.meta.env.VITE_BASE_URL
 export const FILE_URL = import.meta.env.VITE_FILE_URL
 
 // api mock
-export const API_MOCK = import.meta.env.VITE_API_MOCK
+export const API_MOCK = import.meta.env.VITE_API_MOCK === 'true'
 
 // menu mock
-export const MENU_MOCK = import.meta.env.VITE_MENU_MOCK
+export const MENU_MOCK = import.meta.env.VITE_MENU_MOCK === 'true'
 
 // sidebar accordion mode
-export const MENU_ACCORDION = import.meta.env.VITE_MENU_ACCORDITON
+export const MENU_ACCORDION = import.meta.env.VITE_MENU_ACCORDITON === 'true'
