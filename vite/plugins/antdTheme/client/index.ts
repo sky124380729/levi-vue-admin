d402cb82b7e1598d5837932455a12450fdd93c81
zhengquan <zhengquan@inovance.com>
2021-07-14 15:59:14 +0800

    change theme bug fix

--- vite/plugins/antdTheme/client/index.ts
+++ vite/plugins/antdTheme/client/index.ts
@@ -1,15 +1,10 @@
 import { generate } from '@ant-design/colors'
-import { STYLESHEET_ID } from '../index'
 import type { ColorKey } from '../index'
 
 export const replaceCssVars = (key: ColorKey, newColor: string) => {
-    const stylesheet = document.getElementById(STYLESHEET_ID)
-    if (!stylesheet) return
+    const domE = document.documentElement
     const colors = generate(newColor)
     colors.forEach((color, index) => {
-        const reg = new RegExp(`--color-${key}-${index + 1}\s?:(.+)\s?;`, 'g')
-        stylesheet.innerHTML = stylesheet.innerHTML.replace(reg, function (match, p) {
-            return match.replace(new RegExp(p, 'g'), color)
-        })
+        domE.style.setProperty(`--color-${key}-${index + 1}`, color)
     })
 }
